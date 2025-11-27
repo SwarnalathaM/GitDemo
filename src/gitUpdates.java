e7e0e8d5482dd98ed5dfd053fa0f31d3ae74c20c
Swarna <swarnam0302@gmail.com>
2025-11-27 14:48:29 +0530

    1st update from develop branch

--- src/gitUpdates.java
+++ src/gitUpdates.java
@@ -14,7 +14,24 @@ public class gitUpdates {
 	    
 	    System.out.println("4th Update by Person A");
 	    System.out.println("4th Update by Person A");
+	
+	    callBranch1();
+	    callBranch2();
+	    callBranch3();
+	    callBranch4();
+	
 	}
 	
-	
+	public static void callBranch1(){
+    	System.out.println("1st update from develop branch");
+    }
+	public static void callBranch2(){
+    	System.out.println("2nd update from develop branch");
+    }
+	public static void callBranch3(){
+    	System.out.println("3rd update from develop branch");
+    }
+	public static void callBranch4(){
+    	System.out.println("4th update from develop branch");
+    }
 }
